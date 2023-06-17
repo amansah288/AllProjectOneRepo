d89a13a6dc5eb46304bc470a9d1f23cf0c198cf6
aman kumar sah <amansah288@gmail.com>
2023-06-17 15:54:43 +0530

    This is my Third Commit

--- FBLoginPage/app/src/main/java/com/example/fbloginpage/RegisterActivity.java
+++ FBLoginPage/app/src/main/java/com/example/fbloginpage/RegisterActivity.java
@@ -2,7 +2,10 @@ package com.example.fbloginpage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Button SubmitHogya = findViewById(R.id.SubmitHogya);
+
+        SubmitHogya.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openacc();
+            }
+        });
+
+
+    }
+
+    public void openacc()
+    {
+        Intent intent = new Intent(RegisterActivity.this,SuPage.class);
+        startActivity(intent);
     }
 }
---
+++ FBLoginPage/app/src/main/java/com/example/fbloginpage/SuPage.java
@@ -0,0 +1,17 @@
+package com.example.fbloginpage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+public class SuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sucsessfull_register);
+
+    }
+}
